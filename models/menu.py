356e5a9fd1f85e558ee9b153f2be27080d17df47
yurizn18 <duarteyurioliveira@gmail.com>
2026-03-05 20:52:58 -0300

    Atualização Menu

--- models/menu.py
+++ models/menu.py
@@ -1,16 +1,6 @@
 class Hospital:
-
-    def __init__(self):
-        self.nome_paciente = ""
-        self.deficiencia = ""
-
     def menu(self):
         print("HOSPITAL VIVER BEM AUTOATENDIMENTO")
-        print("[Cadastro Do Paciente] - Cadastrar paciente")
-
-        self.nome_paciente = input("Digite o nome do paciente: ")
-        self.deficiencia = input("Tem alguma deficiência? (s/n): ")
-
         print("Menu de serviços")
         print("1 - Consulta médica")
         print("2 - Exames")
@@ -18,10 +8,8 @@ class Hospital:
 
         opcao = input("Escolha uma opção: ")
 
-        print("Paciente:", self.nome_paciente)
-        print("Deficiência:", self.deficiencia)
         print("Serviço escolhido:", opcao)
 
-
+ 
 hospital = Hospital()
 hospital.menu()
