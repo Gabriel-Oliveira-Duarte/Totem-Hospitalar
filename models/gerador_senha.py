4cf9cb5a4dc51146e52cc5c4532782d3e59b8546
yurizn18 <duarteyurioliveira@gmail.com>
2026-03-05 21:03:41 -0300

    Update gerador_senha.py
    Gerador de senha

---
+++ models/gerador_senha.py
@@ -0,0 +1,15 @@
+
+class GeradorSenha:
+    def __init__(self):
+        self.senhas_gerais = 0  
+        self.senhas_prioridade = 0  
+    def gerar_senha(self, tipo: str) -> str:
+     
+        if tipo == 'G':
+            self.senhas_gerais += 1
+            return f"G-{self.senhas_gerais:03d}"
+        elif tipo == 'P':
+            self.senhas_prioridade += 1
+            return f"P-{self.senhas_prioridade:03d}"
+        else:
+            raise ValueError("Tipo inválido. Use 'G' para geral ou 'P' para prioridade.")
